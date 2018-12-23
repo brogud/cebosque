47e09316efc92f78a9a2ce14b572ba6011c4c8ea
brogud <brogudbarrientos@gmail.com>
2018-12-22 21:44:54 -0600

    Css and img

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,11 +2,9 @@
     <div class="header-top">
         <div class="container">
           <div class="row align-items-center">
-              <div class="col-lg-12 col-sm-12 col-12 header-top-right">
-                {!! Form::open(['url' => Request::url(), 'method' => 'get']) !!}
-                
-                  <?php echo Form::select('lang', ['es' => 'Espa&ntilde;ol', 'en' => 'English'], 'es',['onchange' => "this.form.submit()"]); ?>
-
+              <div class="col-lg-12 col-sm-12 col-12 header-top-left">
+                {!! Form::open(['url' => Request::url(), 'method' => 'get', 'class' => '']) !!}
+                    <?php echo Form::select('lang', ['es' => 'Espa&ntilde;ol', 'en' => 'English'], 'es',['onchange' => "this.form.submit()", 'class' => 'form-control']); ?>  
                 {!! Form::close() !!}
               </div>
           </div>			  					
@@ -15,15 +13,12 @@
     <div class="container main-menu">
         <div class="row align-items-center justify-content-between d-flex">
           <div id="logo">
-            <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+            <a href="/"><img src="img/logo.png" alt="" title="" /></a>
           </div>
           <nav id="nav-menu-container">
             <ul class="nav-menu">
-              <li><a href="index.html">Home</a></li>
-              <li><a href="about.html">About</a></li>
-              <li><a href="packages.html">Packages</a></li>
-              <li><a href="hotels.html">Hotels</a></li>
-              <li><a href="insurance.html">Insurence</a></li>
+              <li><a href="/">Inicio</a></li>
+              <!--
               <li class="menu-has-children"><a href="">Blog</a>
                 <ul>
                   <li><a href="blog-home.html">Blog Home</a></li>
@@ -40,8 +35,9 @@
                         </ul>
                       </li>					                		
                 </ul>
+              -->
               </li>					          					          		          
-              <li><a href="contact.html">Contact</a></li>
+            <li><a href="/contact">Contacto</a></li>
             </ul>
           </nav><!-- #nav-menu-container -->					      		  
         </div>
@@ -53,15 +49,15 @@
   <div class="overlay overlay-bg"></div>				
   <div class="container">
       <div class="row fullscreen align-items-center justify-content-between">
-        <div class="col-lg-6 col-md-6 banner-left">
-                  <h6 class="text-white">Away from monotonous life</h6>
-                  <h1 class="text-white">Magical Travel</h1>
+        <div class="col-lg-8 col-md-8 banner-left">
+                  <h6 class="text-white">Lejos de una vida mon&oacute;tona</h6>
+                  <h1 class="text-white">Caba&ntilde;as El Bosque</h1>
                   <p class="text-white">
-                    If you are looking at blank cassettes on the web, you may be very confused at the difference in price. You may see some for as low as $.17 each.
+                    Brindamos un excelente servicio, alojamiento, gastronomía y descanso en un lugar rodeado de naturaleza.
                   </p>
-                  <a href="#" class="primary-btn text-uppercase">Get Started</a>
+                  <a href="#" class="primary-btn text-uppercase">¡Comencemos!</a>
         </div>
-        <div class="col-lg-4 col-md-6 banner-right">
+        <div class="col-lg-4 col-md-4 banner-right">
     
         </div>
     </div>	
