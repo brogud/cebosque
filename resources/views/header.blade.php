cac92802bb597ddb7e42d8ee3344f433ccef0a32
brogud <brogudbarrientos@gmail.com>
2019-01-07 20:50:29 -0600

    Fix

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -19,7 +19,7 @@
             <ul class="nav-menu">
               <li><a href="/">Inicio</a></li>
               </li>	
-              <li class="menu-has-children parent-active"><a href="" class="sf-with-ul">Pages</a>
+              <li class="menu-has-children parent-active"><a href="" class="sf-with-ul">Servicios</a>
                 <ul style="display: none;">
                     <li><a href="#" class="menu-active">Caba&ntilde;as</a></li>
                     <li><a href="/rest" class="menu-active">Restaurante</a></li>
