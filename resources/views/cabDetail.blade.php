a54d1a597e7bf01738755cbb206542b2024872b8
Kenneth Sandoval <kennethsandovalb@gmail.com>
2019-01-07 23:17:50 -0600

    commit

--- resources/views/cabDetail.blade.php
+++ resources/views/cabDetail.blade.php
@@ -21,9 +21,9 @@
                 <div class="row d-flex align-items-center justify-content-center">
                     <div class="about-content col-lg-12">
                         <h1 class="text-white">
-                            Caba&ntilde;a 			
+                            Caba&ntilde;a {{ $cabDetail['cabName'] }}	
                         </h1>	
-                        <p class="text-white link-nav"><a href="/">Inicio </a>  <span class="lnr lnr-arrow-right"></span>  <a href="/contact"> Caba&ntilde;a </a></p>
+                        <p class="text-white link-nav"><a href="/">Inicio </a>  <span class="lnr lnr-arrow-right"></span>  <a href="/contact"> Caba&ntilde;a {{ $cabDetail['cabName'] }}</a></p>
                     </div>	
                 </div>
             </div>
