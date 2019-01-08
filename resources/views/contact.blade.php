6fedab8cf82be6710e99df00f662c96a8e88a242
Kenneth Sandoval <kennethsandovalb@gmail.com>
2019-01-07 20:23:29 -0600

    tilde

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -21,7 +21,7 @@
                 <div class="row d-flex align-items-center justify-content-center">
                     <div class="about-content col-lg-12">
                         <h1 class="text-white">
-                            Contactenos				
+                            Contáctenos				
                         </h1>	
                         <p class="text-white link-nav"><a href="/">Inicio </a>  <span class="lnr lnr-arrow-right"></span>  <a href="/contact"> Contacto</a></p>
                     </div>	
